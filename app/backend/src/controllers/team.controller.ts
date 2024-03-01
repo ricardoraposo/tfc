7566812dff3aa41b2985bb08e5425de443077359
ricardo <ricardorbxx1@gmail.com>
2024-03-01 08:28:38 -0300

    feat: add TeamController layer

---
+++ app/backend/src/controllers/team.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import TeamService from '../services/team.service';
+
+export default class TeamController {
+  constructor(
+    private readonly teamService: TeamService = new TeamService(),
+  ) { }
+
+  async getTeams(_req: Request, res: Response) {
+    try {
+      const { status, data } = await this.teamService.getTeams();
+      return res.status(status).json(data);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+}
