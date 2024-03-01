e9d153a8749c7f5eb3fc18adb35ce7c33b469571
ricardo <ricardorbxx1@gmail.com>
2024-03-01 14:39:37 -0300

    feat: add utility functions to jwt signing and verification

---
+++ app/backend/src/utils/auth.ts
@@ -0,0 +1,19 @@
+import * as jwt from 'jsonwebtoken';
+
+type Payload = {
+  id: number;
+  email: string;
+};
+
+const secret = process.env.JWT_SECRET || 'segredinhoDoRaposo';
+
+export function signToken(payload: Payload) {
+  const token = jwt.sign(payload, secret, { expiresIn: '1d' });
+  return token;
+}
+
+export function verifyToken(token: string) {
+  const payload = jwt.verify(token, secret) as Payload;
+  return payload;
+}
+
