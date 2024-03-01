77778db8bd6a4a69ce68ae7e1dae0d0c17b1c8e8
ricardo <ricardorbxx1@gmail.com>
2024-03-01 10:06:36 -0300

    feat: add getTeamById method to service layer

--- app/backend/src/services/team.service.ts
+++ app/backend/src/services/team.service.ts
@@ -5,8 +5,14 @@ import SequelizeTeamRepository from '../repository/team/sequelizeTeamRepository'
 export default class TeamService {
   constructor(private teamRepository: TeamRepository = new SequelizeTeamRepository()) { }
 
-  public async getTeams() {
+  async getTeams() {
     const teams = await this.teamRepository.getTeams();
     return { status: httpCode.ok, data: teams };
+  }
+
+  async getTeamById(id: number) {
+    const team = await this.teamRepository.getTeamById(id);
+    if (!team) return { status: httpCode.notFound, data: { message: 'Team does not exists' } };
+    return { status: httpCode.ok, data: team };
   }
 }
