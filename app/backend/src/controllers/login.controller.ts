a4d21054aab1dbfb0186a4a6d4aba7a7d5a5ebb2
ricardo <ricardorbxx1@gmail.com>
2024-03-01 15:15:13 -0300

    feat: add getRole method to login controller layer

--- app/backend/src/controllers/login.controller.ts
+++ app/backend/src/controllers/login.controller.ts
@@ -16,4 +16,15 @@ export default class LoginController {
       return res.status(500).json({ message: 'Internal server error' });
     }
   }
+
+  async getRole(_req: Request, res: Response) {
+    const { id } = res.locals.payload;
+    try {
+      const { status, data } = await this.loginService.getUserRole(id);
+      return res.status(status).json(data);
+    } catch (error) {
+      console.error('Error while getting user role', error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  }
 }
