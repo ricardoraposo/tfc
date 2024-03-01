c07245889cf08f918ec06c750dd90c000d90f1da
ricardo <ricardorbxx1@gmail.com>
2024-03-01 15:15:19 -0300

    feat: added a new route

--- app/backend/src/router/login.router.ts
+++ app/backend/src/router/login.router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import LoginController from '../controllers/login.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 import { validateLogin } from '../middlewares/validation.middleware';
 
 const loginController = new LoginController();
@@ -8,5 +9,6 @@ const loginController = new LoginController();
 const router = Router();
 
 router.post('/', validateLogin, (req, res) => loginController.signIn(req, res));
+router.get('/role', authMiddleware, (req, res) => loginController.getRole(req, res));
 
 export default router;
