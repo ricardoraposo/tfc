07acd973856a3c7aefc8a57cb145ef0d0dc88480
ricardo <ricardorbxx1@gmail.com>
2024-03-02 10:47:57 -0300

    feat: added new routes for changing score and status of matches

--- app/backend/src/router/match.router.ts
+++ app/backend/src/router/match.router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import authMiddleware from '../middlewares/auth.middleware';
 import MatchController from '../controllers/match.controller';
 
 const router = Router();
@@ -6,5 +7,17 @@ const router = Router();
 const matchController = new MatchController();
 
 router.get('/', async (req, res) => matchController.getMatches(req, res));
+
+router.patch(
+  '/:id/finish',
+  authMiddleware,
+  async (req, res) => matchController.changeMatchStatus(req, res),
+);
+
+router.patch(
+  '/:id',
+  authMiddleware,
+  async (req, res) => matchController.changeMatchScores(req, res),
+);
 
 export default router;
