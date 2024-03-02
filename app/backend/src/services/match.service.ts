fa563c9d54dad769d7c2af244331449cc111dba7
ricardo <ricardorbxx1@gmail.com>
2024-03-02 11:38:53 -0300

    feat: added createMatch method to match service layer

--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,11 +1,15 @@
 import httpCode from '../utils/httpCode';
+import TeamRepository from '../repository/team/teamRepository';
 import MatchRepository from '../repository/match/matchRepository';
-import SequelizeMatchRepository from '../repository/match/sequelizeMatchRepository';
 import { ChangeMatchScoreDTO } from '../dto/ChangeMatchScoreDTO';
+import SequelizeTeamRepository from '../repository/team/sequelizeTeamRepository';
+import SequelizeMatchRepository from '../repository/match/sequelizeMatchRepository';
+import { CreateMatchDTO } from '../dto/CreateMatchDTO';
 
 export default class MatchService {
   constructor(
     private readonly matchRepository: MatchRepository = new SequelizeMatchRepository(),
+    private readonly teamRepository: TeamRepository = new SequelizeTeamRepository(),
   ) { }
 
   async getMatches(filter: boolean | undefined) {
@@ -26,5 +30,27 @@ export default class MatchService {
   async changeMatchScores(matchId: number, goals: ChangeMatchScoreDTO) {
     await this.matchRepository.changeMatchScores(matchId, goals);
     return { status: httpCode.ok, data: { message: 'Scores changed' } };
+  }
+
+  async createMatch(createMatchDTO: CreateMatchDTO) {
+    if (createMatchDTO.homeTeamId === createMatchDTO.awayTeamId) {
+      return {
+        status: httpCode.unprocessableEntity,
+        data: { message: 'It is not possible to create a match with two equal teams' },
+      };
+    }
+
+    const homeTeam = await this.teamRepository.getTeamById(createMatchDTO.homeTeamId);
+    if (!homeTeam) {
+      return { status: httpCode.notFound, data: { message: 'There is no team with such id!' } };
+    }
+
+    const awayTeam = await this.teamRepository.getTeamById(createMatchDTO.awayTeamId);
+    if (!awayTeam) {
+      return { status: httpCode.notFound, data: { message: 'There is no team with such id!' } };
+    }
+
+    const match = await this.matchRepository.createMatch(createMatchDTO);
+    return { status: httpCode.created, data: match };
   }
 }
