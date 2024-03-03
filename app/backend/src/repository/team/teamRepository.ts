9387fb116603f6dccdc0c2c57b6ea349c645fce1
ricardo <ricardorbxx1@gmail.com>
2024-03-03 20:58:54 -0300

    feat: add getTeamsWithMatches method to team repo layer

--- app/backend/src/repository/team/teamRepository.ts
+++ app/backend/src/repository/team/teamRepository.ts
@@ -3,4 +3,5 @@ import { Team } from '../../Interfaces/Team';
 export default interface TeamRepository {
   getTeams(): Promise<Team[]>;
   getTeamById(id: number): Promise<Team | null>;
+  getTeamsWithMatches(): Promise<Team[]>;
 }
