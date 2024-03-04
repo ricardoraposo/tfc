9a58e5e2f47ba234372987863ca4c8e049124c8f
ricardo <ricardorbxx1@gmail.com>
2024-03-03 21:00:06 -0300

    feat: added extra types to matchmodel

--- app/backend/src/database/models/TeamModel.ts
+++ app/backend/src/database/models/TeamModel.ts
@@ -5,11 +5,14 @@ import {
   InferCreationAttributes,
   CreationOptional,
 } from 'sequelize';
+import { Match } from '../../Interfaces/Match';
 import db from '.';
 
 class TeamModel extends Model<InferAttributes<TeamModel>, InferCreationAttributes<TeamModel>> {
   declare id: CreationOptional<number>;
   declare teamName: string;
+  declare homeMatches?: Match[];
+  declare awayMatches?: Match[];
 }
 
 TeamModel.init({
